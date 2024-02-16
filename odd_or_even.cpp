8e8ead513ee0309890c26dacd36fbd47d557f922
Sachin7717 <sachindhimann71@gmail.com>
2024-02-16 10:35:44 +0530

    check odd or even

---
+++ odd_or_even.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cin>>a;
+     if (a==0)
+    {
+        cout<<"the number is 0";
+    }
+    else if (a%2==0)
+    {
+        cout<<"number is even";
+    }
+    else
+    {
+        cout<<"the number is odd";
+    }
+    
+    
+    
+    return 0;
+}
