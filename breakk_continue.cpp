6c131a17250bd1870bd760de437d8672fb948fce
Sachin7717 <sachindhimann71@gmail.com>
2024-02-16 11:40:59 +0530

    continue statement

--- breakk_continue.cpp
+++ breakk_continue.cpp
@@ -3,16 +3,27 @@ using namespace std;
 
 int main(){
     int i;
-    for (int i = 0; i < 40; i++)
-    {
-        cout<<i<<endl;
-        if (i==4)
-        {
+    // for (int i = 0; i < 40; i++)
+    // {
+    //     cout<<i<<endl;
+    //     if (i==4)
+    //     {
            
-            break;
-        }
+    //         break;
+    //     }
         
+    // }
+     for (int i = 0; i < 40; i++)
+{
+    cout << i << endl;
+    if (i == 4)
+    {
+        continue;
     }
+    cout << i << endl;
+}
+
     return 0;
 }
+    
     
