458f25689faa0ca871fbeee9f31f1f50e92a1a86
Sachin7717 <sachindhimann71@gmail.com>
2024-02-16 11:10:27 +0530

    Loops

---
+++ loopss.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,i=1;
+    cout<<"enter the number";
+
+    cin>>a;
+    // for ( i = 1; i <= 10; i++)
+    // {
+    //     cout<<a*i<<endl;
+        
+    // }
+    while (i<=10)
+    {
+        cout<<a*i<<endl;
+        i++;
+    }
+    
+    
+    return 0;
+}
