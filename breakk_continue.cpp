5ba25dac59f2b2c25e02c94531ff9b2793057324
Sachin7717 <sachindhimann71@gmail.com>
2024-02-16 11:36:59 +0530

    break statement

---
+++ breakk_continue.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i;
+    for (int i = 0; i < 40; i++)
+    {
+        cout<<i<<endl;
+        if (i==4)
+        {
+           
+            break;
+        }
+        
+    }
+    return 0;
+}
+    
