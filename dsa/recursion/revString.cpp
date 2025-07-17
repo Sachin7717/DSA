f51c6ad8e1cfc5081b5a9bdf935ee444803138c5
Sachin7717 <sachindhimann71@gmail.com>
2025-07-17 11:54:58 +0530

    reverse string function

---
+++ dsa/recursion/revString.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    class Solution {
+public:
+    void reverseString(vector<char>& s, int i=0) {
+        int n= s.size();
+        if( i>=n/2) return;
+        swap(s[i],s[n-i-1]);
+
+        reverseString(s,i+1);
+        
+    }
+};
+    return 0;
+}
